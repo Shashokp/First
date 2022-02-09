df629797a1f20489d79b7f605407c333a2bd69ad
Shashokp <shashok.pv@yandex.ru>
2022-02-09 16:14:23 +0700

    test

--- project_1/game.py
+++ project_1/game.py
@@ -2,24 +2,45 @@
 
 import numpy as np
 
-min = 1
-max = 101
+def random_predict() -> int:
+    min = 1
+    max = 101
 
-number = np.random.randint(min, max)
-# number = 77
+    number = np.random.randint(min, max)
+    # number = 77
+    count = 0
+    while True:
+        count+=1
+        mid = round((min+max) / 2)
+        
+        if mid > number:
+            max = mid
+        
+        elif mid < number:
+            min = mid
 
-count = 0
+        else:
+            #print(f"Компьютер угадал число за {count} попыток. Это число {number}")
+            return(count) #break #конец игры выход из цикла   
+        
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
 
-while True:
-    count+=1
-    mid = round((min+max) / 2)
-    
-    if mid > number:
-        max = mid
-    
-    elif mid < number:
-        min = mid
+    Args:
+        random_predict ([type]): функция угадывания
 
-    else:
-        print(f"Компьютер угадал число за {count} попыток. Это число {number}")
-        break #конец игры выход из цикла   
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали списоконлайн переводчик чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")    
+
+if __name__ == '__main__':
+    score_game(random_predict)
